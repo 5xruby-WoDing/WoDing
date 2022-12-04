dd0195079651ac8ae4d0c4a48c36d22edc938a8b
Jeff Chen <zen00326598@gmail.com>
2022-12-04 16:01:44 +0800

    新增新增restaurant model

--- db/schema.rb
+++ db/schema.rb
@@ -47,4 +47,17 @@ ActiveRecord::Schema.define(version: 2022_12_04_074857) do
   end
 
   add_foreign_key "restaurants", "managers"
+  create_table "restaurants", force: :cascade do |t|
+    t.string "title"
+    t.string "tel"
+    t.string "address"
+    t.datetime "delete_at"
+    t.bigint "manager_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["delete_at"], name: "index_restaurants_on_delete_at"
+    t.index ["manager_id"], name: "index_restaurants_on_manager_id"
+  end
+
+  add_foreign_key "restaurants", "managers"
 end
