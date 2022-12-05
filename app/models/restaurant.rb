488746668fbcdfc63a6cfa9a83eb403dd7510aed
Jeff Chen <zen00326598@gmail.com>
2022-12-05 17:02:06 +0800

    restaurant crud

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,9 @@
 class Restaurant < ApplicationRecord
   acts_as_paranoid
   
+  validates :title, presence: true
+  validates :address, presence: true
+  validates :tel, presence: true
+
   belongs_to :manager
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :managers
+
+  resources :managers, only: [:show] do
+    resources :restaurants, shallow: true, only: [:new, :create, :show, :edit, :update, :destroy]
+  end
+  
   root 'home#index'
 end
