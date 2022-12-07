fe132a760182e5b0e6923ccab83a50c98ceafdcc
Yee <a034506618@gmail.com>
2022-12-07 17:31:34 +0800

    backstage Seat Model CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   namespace :backstage do
     resources :managers, only: [:show] do
-      resources :restaurants, shallow: true, only: [:new, :create, :show, :edit, :update, :destroy]   
+      resources :restaurants, shallow: true, only: [:new, :create, :show, :edit, :update, :destroy] do
+        resources :seats, shallow: true, only: [:new, :create, :show, :edit, :update, :destroy]
+      end  
     end
     root 'managers#show'
   end
