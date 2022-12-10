338a38d04d3776e1686dbe391abfbb71f237cc64
Jeff Chen <zen00326598@gmail.com>
2022-12-10 15:28:51 +0800

    合併前台日期按鈕

--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -1,10 +1,28 @@
 module RestaurantsHelper
 
   def run_time(start_time, end_time)
-    return if start_time.to_i >= (end_time).to_i
+    return if start_time.to_i >= end_time.to_i
 
-    @time_list << (start_time + 30.minutes).strftime("%R")
+    @time_list << (start_time + 30.minutes).strftime('%R')
     run_time(start_time + 30.minutes, end_time)
+  end
+
+  def rest_run_time(start_time, end_time)
+    return if start_time.to_i >= end_time.to_i
+
+    @rest_time_list << (start_time + 30.minutes).strftime('%R')
+    rest_run_time(start_time + 30.minutes, end_time)
+  end
+
+  def set_end_time(period)
+    @end_time = Date.today + period.days
+  end
+
+  def date_sort(start_date)
+    return if start_date >= @end_time
+    
+    @date_list << (start_date + 1.days)
+    date_sort(start_date + 1.days)
   end
 
 end
