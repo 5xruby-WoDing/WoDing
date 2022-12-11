35e2749c6ce84d4b3a42cc959a29fc88b51092ae
Jeff Chen <zen00326598@gmail.com>
2022-12-11 16:11:08 +0800

    傳送人數／日期／時間道訂單頁面

---
+++ app/javascript/controllers/time_btn_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "stimulus"
+import log from "tailwindcss/lib/util/log";
+
+export default class extends Controller {
+  static targets = [ '' ]
+
+  connect() {
+  }
+
+  getTime(e){
+    e.preventDefault()
+    e.target.classList.remove("reservation-btn")
+    e.target.classList.add("confirm-state")
+  }
+}
