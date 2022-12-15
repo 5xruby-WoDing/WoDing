eb380966404362a9b7ad63c5d85bcf316f80c8a5
emma <dogwolfpuma@gmail.com>
2022-12-15 23:50:20 +0800

    Tag標籤超連結篩選

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def index
     @restaurants = Restaurant.all
+
+    
+    @restaurants = Restaurant.includes(:tags).where(tags: {name: "#{params[:query]}"}) if params[:query]
   end
 end
