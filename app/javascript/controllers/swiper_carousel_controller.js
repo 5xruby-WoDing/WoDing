5172fcc4b5076cf89f1fb1e9e1da90b8e69e6649
Hsauan <king0833695466@gmail.com>
2022-12-16 18:53:10 +0800

    Modify display picture

--- app/javascript/controllers/swiper_carousel_controller.js
+++ app/javascript/controllers/swiper_carousel_controller.js
@@ -3,24 +3,24 @@ import Swiper from 'swiper/swiper-bundle';
 
 export default class extends Controller {
   connect() {
-    var getSwiperClass = document.querySelector(".mySwiper");
-    if (getSwiperClass){
-      const swiper = new Swiper(".mySwiper", {
-        effect: "coverflow",
-        grabCursor: true,
-        centeredSlides: true,
-        slidesPerView: "auto",
-        coverflowEffect: {
-          rotate: 50,
-          stretch: 0,
-          depth: 50,
-          modifier: 0.5,
-          slideShadows: true,
-        },
-        pagination: {
-          el: ".swiper-pagination",
-        },
-      });
-    }
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      slidesPerGroup: 3,
+      loop: true,
+      loopFillGroupWithBlank: true,
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
   }
 }
