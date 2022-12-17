8e0b2443fac8ab5154fedc2ba7a9b87b36200e2a
Yee <a034506618@gmail.com>
2022-12-18 01:35:15 +0800

    mpg_gateway credit setting

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -3,6 +3,12 @@ class ReservationsController < ApplicationController
   before_action :find_reservation, only: [:checkout]
   
   def checkout
+    @form_info = Newebpay::Mpg.new(@reservation).form_info
+
+    @form_MerchantID = @form_info[:MerchantID]
+    @form_TradeInfo = @form_info[:TradeInfo]
+    @form_TradeSha = @form_info[:TradeSha]
+
   end
 
 
