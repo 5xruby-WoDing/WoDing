3010aaa921c85f1001b5564f93082ff6bbdb02b2
Hsauan <king0833695466@gmail.com>
2022-12-19 17:25:18 +0800

    add READE.md content

--- README.md
+++ README.md
@@ -26,4 +26,9 @@ Things you may want to cover:
 * (2) 加上完成訂單的mail，使用的時候，記得把你自己的mailgun資料填進.env的檔案
 * (3) 如果今天有使用到金流系統，此檔案使用的是藍新金流，記得把env中的三個參數換成自己商店的key
 
+* 拉下檔案的時候，記得把.env.example改成.env，並把自己的環境變數加上去
+* 為了過AWS S3 驗證
+* 檔案內新增加入(1)ACCESS_KEY_ID=貼上key
+* 檔案內新增加入(2)SECRET_ACCESS_KEY=貼上key
+
 * ...
--- app/javascript/controllers/swiper_carousel_top_controller.js
+++ app/javascript/controllers/swiper_carousel_top_controller.js
@@ -3,25 +3,22 @@ import Swiper from 'swiper/swiper-bundle';
 
 export default class extends Controller {
   connect() {
-    const getSwiperTopClass = document.querySelector(".mySwiperTop");
-    if (getSwiperTopClass){
-      var swiperTop = new Swiper(".mySwiperTop", {
-        spaceBetween: 30,
-        effect: "fade",
-        autoplay: {
-          delay: 2000,
-          disableOnInteraction: false,
-        },
-        navigation: {
-          nextEl: ".swiper-button-next-top",
-          prevEl: ".swiper-button-prev-top",
-        },
-        pagination: {
-          el: ".swiper-pagination-top",
-          clickable: true,
-        },
-      });
-    }
+    const swiperTop = new Swiper(".mySwiperTop", {
+      spaceBetween: 30,
+      effect: "fade",
+      autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next-top",
+        prevEl: ".swiper-button-prev-top",
+      },
+      pagination: {
+        el: ".swiper-pagination-top",
+        clickable: true,
+      },
+    });
   }
 }
 
