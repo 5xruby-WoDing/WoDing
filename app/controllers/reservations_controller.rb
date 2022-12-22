9cef4d216d8a91c70838b29002076fa21c947f10
Hsauan <king0833695466@gmail.com>
2022-12-22 18:35:04 +0800

    訂單頁面完成

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,15 +2,22 @@
 
 class ReservationsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:information]
-  before_action :find_reservation, only: [:checkout]
+  before_action :find_reservation, only: [:checkout, :cancel]
 
   def checkout
     @form_info = Newebpay::Mpg.new(@reservation).form_info
-
-    @form_merchant_id = @form_info[:MerchantID]
-    @form_trade_info = @form_info[:TradeInfo]
-    @form_trade_sha = @form_info[:TradeSha]
+    @form_MerchantID = @form_info[:MerchantID]
+    @form_TradeInfo = @form_info[:TradeInfo]
+    @form_TradeSha = @form_info[:TradeSha]
   end
+
+  def cancel
+    if @reservation.may_cancel?
+      @reservation.cancel!
+    end
+    redirect_to root_path, notice: "已成功取消訂單"
+  end
+
 
   def information
     response = Newebpay::MpgResponse.new(params[:TradeInfo])
