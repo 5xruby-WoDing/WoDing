3aef10f5e051f911e5f2595514a90b440b209732
Yee <a034506618@gmail.com>
2022-12-23 21:44:34 +0800

    home search includes restaurants title and tag

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,22 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
+  # def index
+  #   @restaurants = if params[:search]
+  #                    Restaurant.where('title LIKE ?', "%#{params[:search]}%")
+  #                  else
+  #                    Restaurant.all
+  #                  end
+  #   @restaurants = Restaurant.includes(:tags).where(tags: { name: params[:query].to_s }) if params[:query]
+  # end
+
   def index
     @restaurants = if params[:search]
-                     Restaurant.where('title LIKE ?', "%#{params[:search]}%")
+                     Restaurant.joins(:tags).where("title LIKE ?", "%#{params[:search]}%").or(Restaurant.joins(:tags).where("tags.name LIKE ?", "%#{params[:search]}%")).distinct
+                   elsif params[:query]
+                     Restaurant.includes(:tags).where(tags: { name: params[:query].to_s }) 
                    else
                      Restaurant.all
                    end
-    @restaurants = Restaurant.includes(:tags).where(tags: { name: params[:query].to_s }) if params[:query]
   end
 end
