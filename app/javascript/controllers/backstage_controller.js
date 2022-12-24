aa59069915f182704f7b3a0e2100ea17db4d573b
Jeff Chen <zen00326598@gmail.com>
2022-12-24 20:09:32 +0800

    sign in/log in 畫面優化＆icon顯示問題修正

--- app/javascript/controllers/backstage_controller.js
+++ app/javascript/controllers/backstage_controller.js
@@ -1,10 +1,13 @@
 import { Controller } from "stimulus"
-
+import { library, dom} from '@fortawesome/fontawesome-svg-core'
+import { faGear, faLock, faTrash, faPenToSquare, faUtensils, faStore, faPhone, faLocationDot } from '@fortawesome/free-solid-svg-icons'
 export default class extends Controller {
-    static targets = ["info", "reservationInfo", "setting", 'seat']
-
+    static targets = ["info", "reservationInfo", "setting", 'seat', 'dom']
+    initialize(){
+        library.add(faGear, faLock, faTrash, faPenToSquare, faUtensils, faStore, faPhone, faLocationDot )
+    }
     connect() {
-
+        dom.watch()
     }
     info() {
         this.resetBtn()
