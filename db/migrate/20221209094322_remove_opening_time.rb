7e7b4f67b50e0c283639bbef2bb3531d72750785
Jeff Chen <zen00326598@gmail.com>
2022-12-27 18:00:28 +0800

    修改migration

--- db/migrate/20221209094322_remove_opening_time.rb
+++ db/migrate/20221209094322_remove_opening_time.rb
@@ -2,7 +2,6 @@
 
 class RemoveOpeningTime < ActiveRecord::Migration[6.1]
   def change
-    drop_table :opening_times
     add_column :restaurants, :start_time, :time
     add_column :restaurants, :end_time, :time
   end
