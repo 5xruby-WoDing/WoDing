bd347659bc0d0a0f12a261f6f6d97de7a5574302
Yee <a034506618@gmail.com>
2022-12-28 00:58:34 +0800

    revise message content and mpg response reservation state cheange

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
       reservation = Reservation.create!(params_reservation)
       reservation.reserve! if reservation.may_reserve? && reservation.seat.deposit.zero?
 
-      ReserveMailJob.perform_later(reservation)
+      ReserveMailJob.perform_later(reservation) if reservation.seat.deposit == 0
 
       redirect_to checkout_reservation_path(id: reservation.serial)
     else
