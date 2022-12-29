596e1d9c7782281066e748504e0d681e4d776a73
Jeff Chen <zen00326598@gmail.com>
2022-12-29 21:38:17 +0800

    新增公休日功能

---
+++ app/javascript/controllers/off_day_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = ['input', 'date', 'offDay']
+
+  connect(){
+    const offDay = this.offDayTargets.map(a => a.dataset.date)
+    this.dateTargets.forEach(btn => {
+      if(offDay.includes(btn.dataset.date)){
+        btn.classList.add('hidden')
+      }
+    })
+  }
+  getDate(e){
+    this.dateTargets.forEach(btn => btn.classList.add('gray-btn'))
+    this.dateTargets.forEach(btn => btn.classList.remove('major-btn'))
+    e.target.classList.remove('gray-btn')
+    e.target.classList.add('major-btn')
+    this.setDate(e.target.dataset.date)
+  }
+  setDate(date){
+    this.inputTarget.value = date
+  }
+}
