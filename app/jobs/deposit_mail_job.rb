f1e1770fbbca91fbc2fb6f8fc55ee72b6375b00d
Yee <a034506618@gmail.com>
2023-01-06 01:41:44 +0800

    fix mail url

--- app/jobs/deposit_mail_job.rb
+++ app/jobs/deposit_mail_job.rb
@@ -4,7 +4,6 @@ class DepositMailJob < ApplicationJob
   queue_as :default
 
   def perform(reservation)
-    # Do something later
     SeatDepositMailer.deposit_notify(reservation).deliver
   end
 end
--- app/mailers/seat_deposit_mailer.rb
+++ app/mailers/seat_deposit_mailer.rb
@@ -4,7 +4,10 @@ class SeatDepositMailer < ApplicationMailer
   def deposit_notify(reservation)
     @reservation = reservation
     @deposit = reservation.seat.deposit
-
+    @deposit_url = "#{ENV['WEB_DOMAIN']}/reservations/#{reservation.serial}/checkout"
+    # @deposit_url = "http://localhost:3000/reservations/#{reservation.serial}/checkout"
+    
+ 
     mail to: reservation.email, subject: '訂位付款連結，付完款後才會完成訂位'
   end
 end
